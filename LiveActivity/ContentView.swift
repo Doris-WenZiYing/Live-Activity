d0b28f65fe01604a3afd69bd67b899b7028264a4
Doris <doris070714@gmail.com>
2023-12-27 14:43:50 +0800

    Initial Commit

---
+++ LiveActivity/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveActivity
+//
+//  Created by Doris Wen on 2023/12/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiveActivity/LiveActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityApp.swift
+//  LiveActivity
+//
+//  Created by Doris Wen on 2023/12/27.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
