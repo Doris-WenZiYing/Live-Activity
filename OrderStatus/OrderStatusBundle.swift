ba1dd0502209a6ec510dcf180fb8f09b8acae46f
doris <doris070714@gmail.com>
2024-01-11 18:14:01 +0800

    widget first commit

---
+++ OrderStatus/OrderStatusBundle.swift
@@ -0,0 +1,17 @@
+//
+//  OrderStatusBundle.swift
+//  OrderStatus
+//
+//  Created by Doris Wen on 2023/12/27.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct OrderStatusBundle: WidgetBundle {
+    var body: some Widget {
+        OrderStatus()
+        OrderStatusLiveActivity()
+    }
+}
